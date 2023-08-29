3a196b252da7a3e0d61a1d0780a63056a3f393bc
Oscar Fonseca <edsphinx@gmail.com>
2023-08-29 13:37:33 -0600

    feat: password hash implemented

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/users';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -22,10 +23,16 @@ router.post('/api/users/signup', [
 
   const { email, password } = req.body;
 
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
+  const existingUser = await User.findOne({ email });
 
-  res.send({});
+  if (existingUser) {
+    throw new BadRequestError('Email in use ');
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
