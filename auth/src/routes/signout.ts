dd21e312415a8cb30ed3f63e22223813b459bbf8
Oscar Fonseca <edsphinx@gmail.com>
2023-09-07 06:24:26 -0600

    feat: SignOut Implemented

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/signout', (req, res) => {
-  res.send('Hi there!');
+router.post('/api/users/signout', (req, res) => {
+  res.clearCookie('session');
+  res.send({});
 });
 
 export { router as signoutRouter };
